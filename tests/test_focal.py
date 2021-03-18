6cc76d5d5ffd655a081575385c82a5eab99a3650
Dan <daniel.timbrell@ing.com>
2021-03-18 17:01:24 +0100

    test weighted focal loss

---
+++ tests/test_focal.py
@@ -0,0 +1,63 @@
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+from bokbokbok.loss_functions.classification import WeightedFocalLoss
+from bokbokbok.eval_metrics.classification import WeightedFocalMetric
+from bokbokbok.utils import clip_sigmoid
+import lightgbm as lgb
+
+
+def test_focal_lgb_implementation():
+    """
+    Assert that there is no difference between running Focal with alpha=1 and gamma=0
+    and LightGBM's internal CE loss.
+    """
+    X, y = make_classification(n_samples=1000, 
+                               n_features=10, 
+                               random_state=41114)
+
+    X_train, X_valid, y_train, y_valid = train_test_split(X, 
+                                                          y, 
+                                                          test_size=0.25, 
+                                                          random_state=41114)
+
+    alpha = 1.0
+
+    train = lgb.Dataset(X_train, y_train)
+    valid = lgb.Dataset(X_valid, y_valid, reference=train)
+
+    params_wfl = {
+     'n_estimators': 300,
+     'seed': 41114,
+     'n_jobs': 8,
+     'learning_rate': 0.1,
+   }
+
+    wfl_clf = lgb.train(params=params_wfl,
+                train_set=train,
+                valid_sets=[train, valid],
+                valid_names=['train','valid'],
+                fobj=WeightedFocalLoss(alpha=1.0, gamma=0),
+                feval=WeightedFocalMetric(alpha=1.0, gamma=0),
+                early_stopping_rounds=100)
+
+
+    params = {
+        'n_estimators': 300,
+        'objective': 'cross_entropy',
+        'seed': 41114,
+        'n_jobs': 8,
+        'metric': 'cross_entropy',
+        'learning_rate': 0.1,
+        'boost_from_average': False
+    }
+
+    clf = lgb.train(params=params,
+                    train_set=train,
+                    valid_sets=[train, valid],
+                    valid_names=['train','valid'],
+                    early_stopping_rounds=100)
+
+    wfl_preds = clip_sigmoid(wfl_clf.predict(X_valid))
+    preds = clf.predict(X_valid)
+    assert mean_absolute_error(wfl_preds, preds) == 0.0
