866c4addf1af7e1bf1370b47a354e890e0798f07
Dan <daniel.timbrell@ing.com>
2021-03-18 18:21:22 +0100

    compare WCE and Focal

--- tests/test_focal.py
+++ tests/test_focal.py
@@ -1,8 +1,8 @@
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
-from bokbokbok.loss_functions.classification import WeightedFocalLoss
-from bokbokbok.eval_metrics.classification import WeightedFocalMetric
+from bokbokbok.loss_functions.classification import WeightedFocalLoss, WeightedCrossEntropyLoss
+from bokbokbok.eval_metrics.classification import WeightedFocalMetric, WeightedCrossEntropyMetric
 from bokbokbok.utils import clip_sigmoid
 import lightgbm as lgb
 
@@ -22,6 +22,7 @@ def test_focal_lgb_implementation():
                                                           random_state=41114)
 
     alpha = 1.0
+    gamma = 0
 
     train = lgb.Dataset(X_train, y_train)
     valid = lgb.Dataset(X_valid, y_valid, reference=train)
@@ -37,8 +38,8 @@ def test_focal_lgb_implementation():
                 train_set=train,
                 valid_sets=[train, valid],
                 valid_names=['train','valid'],
-                fobj=WeightedFocalLoss(alpha=1.0, gamma=0),
-                feval=WeightedFocalMetric(alpha=1.0, gamma=0),
+                fobj=WeightedFocalLoss(alpha=alpha, gamma=gamma),
+                feval=WeightedFocalMetric(alpha=alpha, gamma=gamma),
                 early_stopping_rounds=100)
 
 
@@ -61,3 +62,59 @@ def test_focal_lgb_implementation():
     wfl_preds = clip_sigmoid(wfl_clf.predict(X_valid))
     preds = clf.predict(X_valid)
     assert mean_absolute_error(wfl_preds, preds) == 0.0
+
+
+def test_focal_wce_comparison():
+    """
+    Assert that there is no difference between running Focal with alpha=3 and gamma=0
+    and running WCE with alpha=3.
+    """
+    X, y = make_classification(n_samples=1000, 
+                               n_features=10, 
+                               random_state=41114)
+
+    X_train, X_valid, y_train, y_valid = train_test_split(X, 
+                                                          y, 
+                                                          test_size=0.25, 
+                                                          random_state=41114)
+
+    alpha = 3.0
+    gamma = 0
+
+    train = lgb.Dataset(X_train, y_train)
+    valid = lgb.Dataset(X_valid, y_valid, reference=train)
+
+    params_wfl = {
+     'n_estimators': 300,
+     'seed': 41114,
+     'n_jobs': 8,
+     'learning_rate': 0.1,
+   }
+
+    wfl_clf = lgb.train(params=params_wfl,
+                train_set=train,
+                valid_sets=[train, valid],
+                valid_names=['train','valid'],
+                fobj=WeightedFocalLoss(alpha=alpha, gamma=gamma),
+                feval=WeightedFocalMetric(alpha=alpha, gamma=gamma),
+                early_stopping_rounds=100)
+
+
+    params_wce = {
+     'n_estimators': 300,
+     'seed': 41114,
+     'n_jobs': 8,
+     'learning_rate': 0.1,
+   }
+
+    wce_clf = lgb.train(params=params_wce,
+                train_set=train,
+                valid_sets=[train, valid],
+                valid_names=['train','valid'],
+                fobj=WeightedCrossEntropyLoss(alpha=alpha),
+                feval=WeightedCrossEntropyMetric(alpha=alpha),
+                early_stopping_rounds=100)
+
+    wfl_preds = clip_sigmoid(wfl_clf.predict(X_valid))
+    wce_preds = clip_sigmoid(wce_clf.predict(X_valid))
+    assert mean_absolute_error(wfl_preds, wce_preds) == 0.0
