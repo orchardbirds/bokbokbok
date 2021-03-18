8da5aa7f99455a0938aac045ff450d3d45ba7984
Dan <daniel.timbrell@ing.com>
2021-03-18 16:19:06 +0100

    assert clip sigmoid values

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,7 @@
 
-from bokbokbok.utils.functions import say_hello
 import numpy as np
 from bokbokbok.utils import clip_sigmoid
 
-# def test_clip_sigmoid():
-#     assert clip_sigmoid(np.array(np.float(0.2342))) == 0
-
-def test_say_hello():
-    assert say_hello() == "hi"
+def test_clip_sigmoid():
+    assert np.allclose(a=clip_sigmoid(np.array([100, 0, -100])),
+                       b=[1 - 1e-15, 0.5, 1e-15])
