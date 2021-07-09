14dc8c245ac4a95597011feeefbec3f008a5e529
Dan Timbrell <49144672+orchardbirds@users.noreply.github.com>
2021-07-09 10:47:03 +0200

    version bump for SPE

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ docs_dep = [
 
 setup(
     name="bokbokbok",
-    version="0.4",
+    version="0.5",
     description="Custom Losses and Metrics for XGBoost, LightGBM, CatBoost",
     long_description=long_description,
     long_description_content_type="text/markdown",
