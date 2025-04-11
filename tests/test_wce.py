845add48cd842b48c60f9b0dee687b69e0106760
danielti <daniel.timbrell@adyen.com>
2025-04-11 09:40:30 +0200

    bump to version 1.0.0, use python3.11

--- tests/test_wce.py
+++ tests/test_wce.py
@@ -109,7 +109,7 @@ def test_wce_xgb_implementation():
 
     params = {
         "seed": 41114,
-        "objective": "binary:logitraw",
+        "objective": "reg:logistic",
         "eval_metric": "logloss",
         "learning_rate": 0.1,
     }
