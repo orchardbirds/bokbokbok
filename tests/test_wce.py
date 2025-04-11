7e3e997ff2829179056be4ae2e071204f8722eb3
danielti <daniel.timbrell@adyen.com>
2025-04-11 09:46:05 +0200

    bump to version 1.0.0, use python3.11

--- tests/test_wce.py
+++ tests/test_wce.py
@@ -88,7 +88,7 @@ def test_wce_xgb_implementation():
     params_wce = {
         "seed": 41114,
         "learning_rate": 0.1,
-        "disable_default_eval_metric": 1
+        "disable_default_eval_metric": True
     }
 
     results = {}
@@ -110,7 +110,7 @@ def test_wce_xgb_implementation():
     params = {
         "seed": 41114,
         "objective": "reg:logistic",
-        "eval_metric": "logloss",
+        #"eval_metric": "logloss",
         "learning_rate": 0.1,
     }
     results = {}
@@ -124,4 +124,7 @@ def test_wce_xgb_implementation():
 
     wce_preds = clip_sigmoid(bst_wce.predict(dvalid))
     preds = bst.predict(dvalid)
+    print(preds)
+    print(bst_wce.predict(dvalid))
+    print(wce_preds)
     assert mean_absolute_error(wce_preds, preds) == 0.0
