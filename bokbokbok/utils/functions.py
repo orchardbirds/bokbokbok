aab95ef212aba7c8d7f11c253d5a8aecac37dac3
danielti <daniel.timbrell@adyen.com>
2025-04-11 14:14:16 +0200

    bump to version 1.0.0, use python3.11

--- bokbokbok/utils/functions.py
+++ bokbokbok/utils/functions.py
@@ -1,19 +1,23 @@
 import numpy as np
+from scipy.special import expit
 
 
-def clip_sigmoid(yhat):
-    """
-    Applies the sigmoid function and ensures that the values lie in the range
-    1e-15 < yhat < 1 - 1e-15.
-    We clip to avoid dividing by zero in the loss functions.
+# def clip_sigmoid(yhat):
+#     """
+#     Applies the sigmoid function and ensures that the values lie in the range
+#     1e-15 < yhat < 1 - 1e-15.
+#     We clip to avoid dividing by zero in the loss functions.
 
-    Args:
-        yhat: The margin probabilities yet to be put into a sigmoid function
+#     Args:
+#         yhat: The margin probabilities yet to be put into a sigmoid function
 
-    Returns:
-        yhat: The clipped probabilities
-    """
-    yhat = 1. / (1. + np.exp(-yhat))
-    yhat[yhat >= 1] = 1 - 1e-6
-    yhat[yhat <= 0] = 1e-6
-    return yhat
+#     Returns:
+#         yhat: The clipped probabilities
+#     """
+#     yhat = 1. / (1. + np.exp(-yhat))
+#     yhat[yhat >= 1] = 1 - 1e-6
+#     yhat[yhat <= 0] = 1e-6
+#     return yhat
+
+def clip_sigmoid(yhat, eps=1e-15):
+    return np.clip(expit(yhat), eps, 1 - eps)
